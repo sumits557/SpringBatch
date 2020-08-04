25bc2bc1866f8b81f70f1d86dd1ad5407572efc6
sumits557 <sumits557@gmail.com>
2020-08-04 15:14:13 -0400

    Added a very basic Spring Batch boot application.

---
+++ spring-batch/src/main/java/com/sumit/springbatch/SpringBatchApplication.java
@@ -0,0 +1,47 @@
+package com.sumit.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchApplication {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step packageItemStep() {
+        return this.stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("This item has been packaged");
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    @Bean
+    public Job deliverPackageJob() {
+        return this.jobBuilderFactory.get("deliverPackageJob").start(packageItemStep()).build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+
+}
